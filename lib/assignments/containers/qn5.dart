5c020422038a11b29f0f10a2d5e44d9d1599dda2
sreeh <sreehariv642@gmail.com>
2023-11-03 09:36:15 +0530

    done

--- lib/assignments/containers/qn5.dart
+++ lib/assignments/containers/qn5.dart
@@ -6,31 +6,11 @@ class Assignment5 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.grey[300],
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              height: 50,
-              width: 50,
-              color: Colors.red,
-            ),
-            Container(
-              height: 50,
-              width: 50,
-              color: Colors.green,
-            ),
-            Container(
-              height: 50,
-              width: 50,
-              color: Colors.blue,
-            ),
-          ],
+        decoration: BoxDecoration(
+          border: Border.all()
         ),
       ),
-    );
+    ) ;
   }
 }
+
